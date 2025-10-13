836399943e6ab56ba24eee6ef3ad84b4ccebff9e
Ruslan <ruslan.konoplya@gmail.com>
2025-10-13 12:39:42 +0300

    add solution

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import { NavLink, Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      <nav className="navbar is-light is-mobile has-shadow" data-cy="Nav">
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                classNames('navbar-item', {
+                  'is-active': isActive,
+                })
+              }
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              to="/tabs"
+              className={({ isActive }) =>
+                classNames('navbar-item', {
+                  'is-active': isActive,
+                })
+              }
+            >
+              Tabs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
